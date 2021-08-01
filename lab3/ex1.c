fcec0e0801277859c3ec918f47892cbb7051c4ea
Gabriel Magalhães <gabriel.magalhaes@netbr.com.br>
2021-08-01 20:34:55 -0300

    Fim da implementacao

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -6,18 +6,48 @@
 long long int tam_vetor;
 int n_threads;
 int *vetor;
-int maior;
+int conc_maior = -1;
+int conc_menor = 100;
+int seq_maior = -1;
+int seq_menor = 100;
 double start, finish, elapsed;
 
 double t_sequencial = 0;
 double t_s = 0;
 double t_p = 0;
 
-void* prod_matrizes(void* arg){
+typedef struct {
+  float menor;
+  float maior;
+} thread_data;
+
+void* verifica_vetor(void* arg){
     int ident = *(int*) arg;
-    // calcular intervalo da matriz pra somar
-    // somar
-    pthread_exit(NULL); // retornar soma
+
+    thread_data* thread_resultado;
+    thread_resultado = (thread_data*) malloc(sizeof(thread_data));
+    thread_resultado->maior = -1;
+    thread_resultado->menor = 100;
+
+    long long int inicio = (tam_vetor/n_threads) * ident;
+    long long int final;
+    if(ident == n_threads - 1){
+        final = tam_vetor;
+    } else {
+        final = (tam_vetor/n_threads) + inicio;
+    }
+    
+    for(int i = inicio; i < final; i++){
+        if(vetor[i] > thread_resultado->maior){
+            thread_resultado->maior = vetor[i];
+        }
+        if(vetor[i] < thread_resultado->menor) {
+            thread_resultado->menor = vetor[i];
+        }
+    }
+
+    pthread_exit((void*) thread_resultado);
+    return(NULL);
 }
 
 int main(int argc, char* argv[]){
@@ -36,6 +66,7 @@ int main(int argc, char* argv[]){
 
     pthread_t tid[n_threads];
     int ident[n_threads];
+    thread_data* thread_resultado;
     
     for(int i = 0; i < tam_vetor; i++){
         vetor[i] = i % 10;
@@ -52,16 +83,25 @@ int main(int argc, char* argv[]){
 
     for(int i = 0; i < n_threads; i++){
         ident[i] = i;
-        if(pthread_create(&tid[i], NULL, prod_matrizes, (void *) &ident[i])){
+        if(pthread_create(&tid[i], NULL, verifica_vetor, (void *) &ident[i])){
             printf("ERRO NA CRIACAO DA THREAD");
         }
     }
 
     for(int i = 0; i < n_threads; i++){
         ident[i] = i;
-        if(pthread_join(tid[i], NULL)){
+        if(pthread_join(tid[i], (void**) &thread_resultado)){
             printf("ERRO NO JOIN DAS THREADS");
         }
+
+        if(thread_resultado->maior > conc_maior){
+            conc_maior = thread_resultado->maior;
+        }
+        if(thread_resultado->menor < conc_menor){
+            conc_menor = thread_resultado->menor;
+        }
+
+        free(thread_resultado);
     }
 
     GET_TIME(finish);
@@ -73,8 +113,11 @@ int main(int argc, char* argv[]){
     GET_TIME(start);
 
     for(int i = 0; i < tam_vetor; i++){
-        if(vetor[i] > maior){
-            maior = vetor[i];
+        if(vetor[i] > seq_maior){
+            seq_maior = vetor[i];
+        }
+        if(vetor[i] < seq_menor) {
+            seq_menor = vetor[i];
         }
     }
 
@@ -84,6 +127,10 @@ int main(int argc, char* argv[]){
     t_sequencial += elapsed;
 
     // Teste
+
+    // if((seq_maior == conc_maior) && (seq_menor == conc_menor)){
+    //     puts("Passou no teste.");
+    // }
 
 
     // Limpeza de espaco alocado do vetor

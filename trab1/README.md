48a61d7d4c741be1d7971d941be3aa8df8b508a6
mesquitaaph <raphafm.rf@gmail.com>
2021-08-09 12:58:02 -0300

    mandelbrot selta

--- trab1/README.md
+++ trab1/README.md
@@ -5,7 +5,7 @@
 	- glfw3
 	
 Para compilar use: \
-`gcc main.c -o main -Wall -lpthread -lglfw -lGLU -lGL` 
+`gcc main.c -o main -Wall -lpthread -lglfw -lGLU -lGL -lm` 
 
 Para instalar a biblioteca GLFW no Ubuntu use o comando: \
 `sudo apt-get install libglfw3 libglfw3-dev`
--- trab1/main.c
+++ trab1/main.c
@@ -1,4 +1,31 @@
+#include <stdio.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
+
+#define WIDTH 600
+#define HEIGHT 600
+#define MAX_ITERATIONS 10000
+
+GLubyte PixelBuffer[WIDTH * HEIGHT * 3];
+
+void display(GLFWwindow *window) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, PixelBuffer);
+    glfwSwapBuffers(window);
+}
+
+void makePixel(int x, int y, int r, int g, int b, GLubyte* pixels, int width, int height) {
+    if (0 <= x && x < width && 0 <= y && y < height) {
+        int position = (x + y * width) * 3;
+        pixels[position] = r;
+        pixels[position + 1] = g;
+        pixels[position + 2] = b;
+    }
+}
+
+double map(double value, double min, double max, double floor, double ceil) {
+    return floor + (ceil - floor) * ((value - min) / (max - min));
+}
 
 int main(void)
 {
@@ -9,7 +36,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Mandelbrot", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Mandelbrot", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,17 +46,67 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    double zoom = 0.001;
+    double XOffset = -1.25;
+    double YOffset = 0.041;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        // Setup View
+        int width, height;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
         /* Render here */
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                double a = map(x, 0, width, -2.5 * zoom + XOffset, 0.5 * zoom + XOffset);
+                double b = map(y, 0, height, -1.5 * zoom + YOffset, 1.5 * zoom + YOffset);
+                
+                double ca = a;
+                double cb = b;
+
+                int n = 0;
+
+                while (n < MAX_ITERATIONS) {
+                    double aa = a*a - b*b;
+                    double bb = 2 * a * b;
+
+                    a = aa + ca;
+                    b = bb + cb;
+                    if(a + b > 16 || a + b < -16) {
+                        break;
+                    }
+                    n++;
+                }
+
+                double bright = map(n, 0, MAX_ITERATIONS, 0, 1);
+                bright = map(sqrt(bright), 0, 1, 0, 255);
+                if(n == MAX_ITERATIONS) {
+                    bright = 0;
+                }
+                makePixel(
+                    x, y, 
+                    bright, bright, bright,
+                    PixelBuffer, WIDTH, HEIGHT
+                );
+            }
+        }
+
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Drawing
+        glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, PixelBuffer);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
+        printf("FRAME FIM --> zoom %lf\n", zoom);
+        // break;
+        zoom *= 0.75;
     }
 
     glfwTerminate();

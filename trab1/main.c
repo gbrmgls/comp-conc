9ebf90376f337fbd4e91042a004f41f97e41f8bd
mesquitaaph <raphafm.rf@gmail.com>
2021-08-30 18:43:12 -0300

    correções e comentários

--- trab1/main.c
+++ trab1/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
 #include <pthread.h>
@@ -6,14 +7,16 @@
 
 #define WIDTH 600
 #define HEIGHT 600
-#define MAX_ITERATIONS 10000
-#define N_THREADS 12
+// #define MAX_ITERATIONS 1000000
+// #define N_THREADS 12
 
+// Tipo que armazena as coordenadas de um pixel
 typedef struct mtx_coords {
     int x;
     int y;
 } t_coords;
 
+// Tipo que armazena configurações do plot da fractal na janela
 typedef struct window_plot {
     double zoom;
     double XOffset;
@@ -22,87 +25,127 @@ typedef struct window_plot {
 } t_plot;
 
 GLubyte PixelBuffer[WIDTH * HEIGHT * 3];
-t_coords FractalMatrixLocation;
-t_plot WindowMatrixPlot = {0.001, -1.25, 0.041, WIDTH, HEIGHT};
+
+// Matrizes para testar a corretude da forma concorrente do programa
+int mtxtestseq[WIDTH * HEIGHT * 3];
+int mtxtestconc[WIDTH * HEIGHT * 3];
+
+// Atribuindo configurações do programa
+t_plot WindowMatrixPlot = {0.001, -1.2506, 0.041, WIDTH, HEIGHT};
+int N_THREADS;
+int MAX_ITERATIONS;
+double MAX_BRIGHT_LENGTH;
+
+// Variável que armazena o andamento do programa na matriz
+int mtxposition;
 pthread_mutex_t lock;
+
 double start, finish, elapsed;
 
-void nextMatrixLocation() {
-    int x = FractalMatrixLocation.x + 1;
-    int y = FractalMatrixLocation.y;
-    if(x == WIDTH) {
-        x = 0;
-        y++;
-    }
-    FractalMatrixLocation.x = x;
-    FractalMatrixLocation.y = y;
+// Recebe o "andamento" da matriz e define a coordenada de onde está
+void nextMatrixLocation(int mtxposition, t_coords* fractalMatrixLocation) {
+    int x = mtxposition % WIDTH;
+    int y = mtxposition / WIDTH;
+
+    fractalMatrixLocation->x = x;
+    fractalMatrixLocation->y = y;
 }
 
+// Recebe um valor pertencente a um intervalo [min,max] e retorna o valor transformado
+// para o intervalo [floor,ceil]
 double map(double value, double min, double max, double floor, double ceil) {
     return floor + (ceil - floor) * ((value - min) / (max - min));
 }
 
+// "Pinta" os pixels, definidos em PixelBuffer, na janela do programa
 void display(GLFWwindow *window) {
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, PixelBuffer);
     glfwSwapBuffers(window);
 }
 
+// Atribui ao pixel na posição (x,y) a cor [r,g,b]
 void makePixel(int x, int y, int r, int g, int b, GLubyte* pixels, int width, int height) {
     if (0 <= x && x < width && 0 <= y && y < height) {
         int position = (x + y * width) * 3;
         pixels[position] = r;
-        pixels[position + 1] = g;
+        pixels[position + 1] = 0;
         pixels[position + 2] = b;
     }
 }
 
+
+// Função que monta a fractal de mandelbrot na forma sequencial
 void mandelbrotSequencial() {
-    /* Render here */
-        for(int x = 0; x < WindowMatrixPlot.width; x++) {
-            for(int y = 0; y < WindowMatrixPlot.height; y++) {
-                double a = map(x, 0, WindowMatrixPlot.width, -2.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset, 0.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset);
-                double b = map(y, 0, WindowMatrixPlot.height, -1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset, 1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset);
-                
-                double ca = a;
-                double cb = b;
+    for(int x = 0; x < WindowMatrixPlot.width; x++) {
+        for(int y = 0; y < WindowMatrixPlot.height; y++) {
+            double a = map(x, 0, WindowMatrixPlot.width,
+                -2.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset,
+                0.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset);
+            double b = map(y, 0, WindowMatrixPlot.height,
+                -1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset,
+                1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset);
+            
+            double ca = a;
+            double cb = b;
 
-                int n = 0;
+            int n = 0;
 
-                while (n < MAX_ITERATIONS) {
-                    double aa = a*a - b*b;
-                    double bb = 2 * a * b;
+            while (n < MAX_ITERATIONS) {
+                double aa = a*a - b*b;
+                double bb = 2 * a * b;
 
-                    a = aa + ca;
-                    b = bb + cb;
-                    if(a + b > 16 || a + b < -16) {
-                        break;
-                    }
-                    n++;
+                a = aa + ca;
+                b = bb + cb;
+                if(a*a + b*b > 4) {
+                    break;
                 }
-
-                double bright = map(n, 0, MAX_ITERATIONS, 0, 1);
-                bright = map(sqrt(bright), 0, 1, 0, 255);
-                if(n == MAX_ITERATIONS) {
-                    bright = 0;
-                }
-                makePixel(
-                    x, y, 
-                    bright, bright, bright,
-                    PixelBuffer, WIDTH, HEIGHT
-                );
+                n++;
             }
+            
+            // Controla o brilho de cada pixel, dependendo do número de iterações
+            double bright = map(n, 0, MAX_ITERATIONS, 0, MAX_BRIGHT_LENGTH);
+            bright = map(sqrt(bright), 0, MAX_BRIGHT_LENGTH, 0, 255);
+            if(n == MAX_ITERATIONS) {
+                bright = 0;
+            }
+
+            // Pinta os pixels na janela
+            // makePixel(
+            //     x, y, 
+            //     bright, bright, bright,
+            //     PixelBuffer, WIDTH, HEIGHT
+            // );
+
+            // Monta a matriz de pixels da forma sequencial para testar no final
+            int position = (x + y * WIDTH) * 3;
+            mtxtestseq[position] = bright;
+            mtxtestseq[position + 1] = bright;
+            mtxtestseq[position + 2] = bright;
         }
+    }
 }
 
 void* calculaPixel(void* arg) {
-    while(FractalMatrixLocation.y < HEIGHT) {
+    t_coords FractalMatrixLocation;
+    int threadPosition;
+    while(1) {
+        // Soma uma unidade ao "andamento" da matriz e informa qual posição a thread deve calular
+        // impedindo que outra thread some o andamento antes da thread ser informada.
         pthread_mutex_lock(&lock);
-        nextMatrixLocation();
+        mtxposition++;
+        threadPosition = mtxposition;
         pthread_mutex_unlock(&lock);
-        // printf("x = %d e y = %d\n", FractalMatrixLocation.x, FractalMatrixLocation.y);
-        double a = map(FractalMatrixLocation.x, 0, WindowMatrixPlot.width, -2.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset, 0.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset);
-        double b = map(FractalMatrixLocation.y, 0, WindowMatrixPlot.height, -1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset, 1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset);
+
+        nextMatrixLocation(threadPosition, &FractalMatrixLocation);
+        if(threadPosition >= WIDTH * HEIGHT) break;
+
+        double a = map(FractalMatrixLocation.x, 0, WindowMatrixPlot.width,
+            -2.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset,
+            0.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.XOffset);
+        double b = map(FractalMatrixLocation.y, 0, WindowMatrixPlot.height,
+            -1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset,
+            1.5 * WindowMatrixPlot.zoom + WindowMatrixPlot.YOffset);
         
         double ca = a;
         double cb = b;
@@ -115,37 +158,56 @@ void* calculaPixel(void* arg) {
 
             a = aa + ca;
             b = bb + cb;
-            if(a + b > 16 || a + b < -16) {
+            if(a*a + b*b > 4) {
                 break;
             }
             n++;
         }
 
-        double bright = map(n, 0, MAX_ITERATIONS, 0, 1);
-        bright = map(sqrt(bright), 0, 1, 0, 255);
+        // Controla o brilho de cada pixel, dependendo do número de iterações
+        double bright = map(n, 0, MAX_ITERATIONS, 0, MAX_BRIGHT_LENGTH);
+        bright = map(sqrt(bright), 0, MAX_BRIGHT_LENGTH, 0, 255);
         if(n == MAX_ITERATIONS) {
             bright = 0;
         }
+
+        // Pinta os pixels na janela
         makePixel(
             FractalMatrixLocation.x, FractalMatrixLocation.y, 
             bright, bright, bright,
             PixelBuffer, WIDTH, HEIGHT
         );
+
+        // Monta a matriz de pixels da forma concorrente para testar no final
+        int position = (FractalMatrixLocation.x + FractalMatrixLocation.y * WIDTH) * 3;
+        mtxtestconc[position] = bright;
+        mtxtestconc[position + 1] = bright;
+        mtxtestconc[position + 2] = bright;
     }
     pthread_exit(NULL);
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    if(argc < 3){
+        printf("Execute: %s <numero maximo de iteracoes> <numero de threads>", argv[0]);
+        return 1;
+    }
+
+    N_THREADS = atoi(argv[2]);
+    MAX_ITERATIONS = atoi(argv[1]);
+
+    MAX_BRIGHT_LENGTH = MAX_ITERATIONS < 1000 ? 1 : 10000.0/MAX_ITERATIONS;
+
     pthread_t threads[N_THREADS];
     pthread_mutex_init(&lock, NULL);
 
     GLFWwindow* window;
-    /* Initialize the library */
+    // Inicializando a biblioteca
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Criando a janela e seu contexto OpenGL
     window = glfwCreateWindow(WIDTH, HEIGHT, "Mandelbrot", NULL, NULL);
     if (!window)
     {
@@ -153,26 +215,19 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    // Cria o contexto atual da janela
     glfwMakeContextCurrent(window);
 
-    // double zoom = 0.001;
-    // double XOffset = -1.25;
-    // double YOffset = 0.041;
     int frame = 1;
-    /* Loop until the user closes the window */
+
     GET_TIME(start);
     while (!glfwWindowShouldClose(window))
     {
-        // Setup View
-        // int width, height;
-        FractalMatrixLocation.x = -1;
-        FractalMatrixLocation.y = 0;
-
+        // Configuração da visualização
+        mtxposition = -1;
         glfwGetFramebufferSize(window, &WindowMatrixPlot.width, &WindowMatrixPlot.height);
         glViewport(0, 0, WindowMatrixPlot.width, WindowMatrixPlot.height);
 
-        /* Render here */
         for(int i = 0; i < N_THREADS; i++) {
             if(pthread_create(&threads[i], NULL, calculaPixel, NULL)) {
                 printf("Erro pthread_create");
@@ -187,60 +242,37 @@ int main(void)
             }
         }
         // mandelbrotSequencial();
-        // for(int x = 0; x < width; x++) {
-        //     for(int y = 0; y < height; y++) {
-        //         double a = map(x, 0, width, -2.5 * zoom + XOffset, 0.5 * zoom + XOffset);
-        //         double b = map(y, 0, height, -1.5 * zoom + YOffset, 1.5 * zoom + YOffset);
-                
-        //         double ca = a;
-        //         double cb = b;
 
-        //         int n = 0;
-
-        //         while (n < MAX_ITERATIONS) {
-        //             double aa = a*a - b*b;
-        //             double bb = 2 * a * b;
-
-        //             a = aa + ca;
-        //             b = bb + cb;
-        //             if(a + b > 16 || a + b < -16) {
-        //                 break;
-        //             }
-        //             n++;
-        //         }
-
-        //         double bright = map(n, 0, MAX_ITERATIONS, 0, 1);
-        //         bright = map(sqrt(bright), 0, 1, 0, 255);
-        //         if(n == MAX_ITERATIONS) {
-        //             bright = 0;
-        //         }
-        //         makePixel(
-        //             x, y, 
-        //             bright, bright, bright,
-        //             PixelBuffer, WIDTH, HEIGHT
-        //         );
-        //     }
-        // }
         frame++;
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Drawing
+        // Desenhando
         glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, PixelBuffer);
 
-        /* Swap front and back buffers */
+        // Funções necesárias para o funcionamento da biblioteca que desenha os pixels
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
-        // printf("FRAME FIM --> zoom %lf\n", WindowMatrixPlot.zoom);
-        // break;
+
         WindowMatrixPlot.zoom *= 0.75;
-        if(frame == 11) break;
+        if(frame == 100) break;
     }
     GET_TIME(finish);
     elapsed = finish - start;
+
     // printf("Tempo de execucao sequencial: %lf\n", elapsed);
     printf("Tempo de execucao concorrente: %lf\n", elapsed);
+
+    int deuRuim = 0;
+    for(int i = 0; i < WIDTH * HEIGHT * 3; i++) {
+        if(mtxtestconc[i] != mtxtestseq[i]) {
+            deuRuim = 1;
+            printf("deu ruim\n");
+            printf("pixel %d %d\n", (i/3)%WIDTH, (i/3)/WIDTH);
+            printf("mtxtestconc[i] = %d, mtxtestseq[i] = %d\n", mtxtestconc[i], mtxtestseq[i]);
+            break;
+        }
+    }
+    if(!deuRuim) printf("Deu bom\n");
 
     pthread_mutex_destroy(&lock);
     glfwTerminate();
